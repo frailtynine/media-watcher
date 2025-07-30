f51451bf55411fd051e01e3800f125ca2e206786
frailtynine <frailty.nine@gmail.com>
2025-07-30 11:28:36 +0100

    Change prompt

--- ai_news_bot/ai/prompts.py
+++ ai_news_bot/ai/prompts.py
@@ -1,16 +1,16 @@
 
 class Prompts:
     POST_EXAMPLE_ONE = """
-        There’s no missing minute in Epstein jail tape: CBS 
+        There’s no missing minute in Epstein jail tape: CBS
         Turns out, if CBS sources are correct, Trump administration has
-        lied about this tape. According to Pam Bondi, there’s a missing minute 
-        at 11:49 pm on the night of Jeffrey Epstein death due to 
-        camera reset protocol. 
+        lied about this tape. According to Pam Bondi, there’s a missing minute
+        at 11:49 pm on the night of Jeffrey Epstein death due to
+        camera reset protocol.
 
         Government sources say it’s bullshit, and all the officials
-        have the original, unedited, video. 
+        have the original, unedited, video.
 
-        What’s happening during this minute is unknown. 
+        What’s happening during this minute is unknown.
         But the pressure to release more Epstein materials is growing on Trump.
         [Will he? Predict on Futurum.](https://t.me/ft_rm_bot/futurum
         ?startapp=event_4e022ad0-28e5-4b3d-bac8-e27ac0716c9f=source_futurumTg)"
@@ -40,8 +40,9 @@ class Prompts:
         You are an assistant for an SMM manager at Polymarket.
         Your goal is to suggest a post for the given breaking news item.
         The post should be concise and engaging and include the link
-        to the market. The ultimate goal is to drive traffic to the market,
-        so call to action is important.
+        to the market. The goal is to inform the audience 
+        and drive traffic to the market. Be informative and 
+        base the post on the news item.
         Check the previous relevant news for the market for the context,
         they will be provided below.
         Check the examples of posts for the context:
